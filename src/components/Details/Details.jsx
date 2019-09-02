10648bda488b54e5b29b531d35869210947fa446
Alex Smith <alexesmith@icloud.com>
2019-09-02 03:09:18 -0500

    setup buttons on details component so it now leads back to the home page and into the edit page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
+
 
 class Details extends Component {
     componentDidMount() {
@@ -23,6 +24,10 @@ class Details extends Component {
         
         return (
            <>
+            <div>
+                <Button variant="contained" color="primary" onClick={() => { this.props.history.push('/') }} className="btn btn-secondary btn-lg checkoutBtn">Home</Button>
+                <Button variant="contained" color="secondary" onClick={() => this.props.history.push(`/Edit/${this.props.match.params.id}`)} className="btn btn-secondary btn-lg checkoutBtn">Edit</Button>
+            </div>
            {/* {JSON.stringify(this.props.reduxStore.specificMovie)} */}
            <img
            src={this.props.reduxStore.specificMovie.poster}
@@ -31,6 +36,7 @@ class Details extends Component {
            <h1>{this.props.reduxStore.specificMovie.title}</h1>
            <p>{this.props.reduxStore.specificMovie.genres}</p>
            <p>{this.props.reduxStore.specificMovie.description}</p>
+           
            
            </>
         );
