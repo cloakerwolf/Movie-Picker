dab01326e5518a0c3d70c2d04ce0e21daf6bd379
Alex Smith <alexesmith@icloud.com>
2019-09-01 22:50:58 -0500

    added some comments

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,28 +1,31 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 class Movie extends Component {
-    seeDetails = (id) => {
-        this.props.history.push('/Details');
-        // this.props.dispatch({
-        //     type: 'FETCH_MOVIE_ID',
-        //     payload: id
-        // })
+    seeDetails = (event) => {
+        //capture the id of the specific movie and push to the details page
+        let id = this.props.movie.id;
+        this.props.history.push(`/Details/${id}`);
+        
     }
 
 
     render(){
         return(
             <div >
+                {/* show posters on dom */}
                 <img src={this.props.movie.poster} 
                 alt={this.props.movie.title}
-                onClick={()=> this.seeDetails(this.props.movie.id)}
+                onClick={this.seeDetails}
                 ></img>
+                {/* show the title of the movies */}
                 <h1>{this.props.movie.title}</h1>
+                {/* show the descriptions on dom */}
                 <p>{this.props.movie.description}</p>
             </div>
         )
     }
 }
 
-export default withRouter(Movie);
+export default withRouter(connect()(Movie);
